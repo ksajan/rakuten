f6db3b163cc23ddd38d8e83d0b020dde049783c1
Sajan Kumar <ksajan12@gmail.com>
2022-07-23 03:50:19 +0530

    updated new api

---
+++ api/routes/response_time.py
@@ -0,0 +1,16 @@
+
+from fastapi import APIRouter, HTTPException, Depends, BackgroundTasks
+from fastapi.responses import JSONResponse
+from fastapi.params import Form, Body
+from fastapi import File, UploadFile
+from pydantic import BaseModel
+
+import requests
+
+response_time_router = APIRouter(prefix="/api/v1", tags=["response_time"])
+
+@response_time_router.post("/response_time/calc_response_time")
+async def calc_response_time(api_endpoint: str, api_method: str):
+    response = f"{requests}.{api_method}({api_endpoint})"
+    elasped_time = response.elapsed.total_seconds()
+    return JSONResponse({"response_time": elasped_time})
