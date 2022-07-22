40c5bd687ed8f158b3ea9393dad1582401c1aedb
Sajan Kumar <ksajan12@gmail.com>
2022-07-23 04:51:42 +0530

    updated log metrics  and requirements file

--- api/routes/log_metrics.py
+++ api/routes/log_metrics.py
@@ -8,13 +8,13 @@ from pydantic import BaseModel
 
 log_metrics_router = APIRouter(prefix="/api/v1", tags=["log_metrics"])
 
-@log_metrics_router.post("/log_metrics/upload_log_file")
-async def upload_log_file():
-    return JSONResponse({"message": "Upload log file"})
+# @log_metrics_router.post("/log_metrics/upload_log_file")
+# async def upload_log_file():
+#     return JSONResponse({"message": "Upload log file"})
 
-@log_metrics_router.get("/log_metrics/get_log_file")
-async def get_log_file():
-    return JSONResponse({"message": "Get log file"})
+# @log_metrics_router.get("/log_metrics/get_log_file")
+# async def get_log_file():
+#     return JSONResponse({"message": "Get log file"})
 
 @log_metrics_router.get("/log_metrics/parse_android_log_file")
 async def parse_android_log_file():
