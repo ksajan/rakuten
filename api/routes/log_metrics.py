2fcc9345503852826beb24d5589bc4c4ebae9ec7
ksajan <ksajan12@gmail.com>
2022-07-23 11:25:49 +0530

    updated new header

--- api/routes/log_metrics.py
+++ api/routes/log_metrics.py
@@ -18,11 +18,11 @@ log_metrics_router = APIRouter(prefix="/api/v1", tags=["log_metrics"])
 
 @log_metrics_router.get("/log_metrics/parse_android_log_file")
 async def parse_android_log_file():
-    return pd.read_csv("https://rakhuten.s3.ap-south-1.amazonaws.com/data/data/csv_data/Andriod/Andriod_2k.log_structured.csv").to_json()
+    return pd.read_csv("https://rakhuten.s3.ap-south-1.amazonaws.com/data/data/csv_data/Andriod/Andriod_2k.log_structured.csv").head(10).to_json()
 
 @log_metrics_router.get("/log_metrics/parse_apache_log_file")
 async def parse_apache_log_file():
-    return pd.read_csv("https://rakhuten.s3.ap-south-1.amazonaws.com/data/data/csv_data/Apache/Apache_2k.log_structured.csv").to_json()
+    return pd.read_csv("https://rakhuten.s3.ap-south-1.amazonaws.com/data/data/csv_data/Apache/Apache_2k.log_structured.csv").head(10).to_json()
 
 @log_metrics_router.get("/system_metrics/query_log_file")
 async def query_apache_log_file(query_column, query_value, log_type):
@@ -30,5 +30,8 @@ async def query_apache_log_file(query_column, query_value, log_type):
         df = pd.read_csv("https://rakhuten.s3.ap-south-1.amazonaws.com/data/data/csv_data/Apache/Apache_2k.log_structured.csv")
     else:
         df = pd.read_csv("https://rakhuten.s3.ap-south-1.amazonaws.com/data/data/csv_data/Andriod/Andriod_2k.log_structured.csv")
+    if query_column == "Date" or query_column == "Time":
+        query_df = df.query(f"{query_column} == '{query_value}'")
+        return JSONResponse({"query_df": query_df.to_json()})
     query_df = df.query(f"{query_column} == '{query_value}'")
     return JSONResponse({"query_df": query_df.to_json()})
