1e6ee25286ee09531243bef7dc6608fd79538d73
ksajan <ksajan12@gmail.com>
2022-07-23 11:12:58 +0530

    bug fix

--- api/routes/log_metrics.py
+++ api/routes/log_metrics.py
@@ -27,8 +27,8 @@ async def parse_apache_log_file():
 @log_metrics_router.get("/system_metrics/query_log_file")
 async def query_apache_log_file(query_column, query_value, log_type):
     if log_type == "apache":
-        df = pd.read_csv("api/data/Apache/apache_2k.log_structured.csv")
+        df = pd.read_csv("https://rakhuten.s3.ap-south-1.amazonaws.com/data/data/csv_data/Apache/Apache_2k.log_structured.csv")
     else:
-        df = pd.read_csv("api/data/Andriod/Andriod_2k.log_structured.csv")
+        df = pd.read_csv("https://rakhuten.s3.ap-south-1.amazonaws.com/data/data/csv_data/Andriod/Andriod_2k.log_structured.csv")
     query_df = df.query(f"{query_column} == '{query_value}'")
     return JSONResponse({"query_df": query_df.to_json()})
